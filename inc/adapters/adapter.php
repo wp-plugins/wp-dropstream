7d70c1341e911120141be6ef1922566aca07a23d
karlfalconer <karlfalconer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-05 12:10:37 +0000

    Tagging version 0.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-dropstream/tags/0.0.2@723043 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/adapters/adapter.php
+++ inc/adapters/adapter.php
@@ -2,7 +2,7 @@
 interface iAdapter {
   public function getOrders($status);
   public function updateOrderStatus($id, $status);
-  public function createOrderTracking($id, $tracking_number, $send_email = true);
+  public function createOrderTracking($id, $tracking_number, $carrier, $send_email = true);
   public function updateProductInventory($product_id, $sku, $quantity);
 }
 
