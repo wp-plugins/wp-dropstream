e78e87b5d67ad32ee742e011c65612d5ae64d237
karlfalconer <karlfalconer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-04 20:27:29 +0000

    Tagging version 0.8.3
    git-svn-id: https://plugins.svn.wordpress.org/wp-dropstream/tags/0.8.3@1059790 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-dropstream.php
+++ wp-dropstream.php
@@ -3,7 +3,7 @@
 Plugin Name: WP-Dropstream
 Plugin URI: http://getdropstream.com/merchants
 Description: A brief description of the Plugin.
-Version: 0.8.2.1
+Version: 0.8.3
 Author: Dropstream
 Author URI: http://getdropstream.com
 License: http://getdropstream.com/terms
